79fa7407f87db8b3c3bab6889631bdb7ecd59d6a
Shyam Agarwal <shyamagarwal3800@gmail.com>
2024-07-20 00:14:40 +0530

    add gat all reviews controller and fixed some bug in that the review.controllers

--- src/controllers/review.controllers.js
+++ src/controllers/review.controllers.js
@@ -8,7 +8,7 @@ import {Book} from "../models/book.models.js";
 // controller for submiting a review
 const submitReview = asyncHandler(async (req,res)=>{
     const {comment , rating} = req.body
-    const { bookId } = req.params
+    const { bookId } = req.params;
 
     if(!bookId){
         throw new ApiError(401,"Invalid Request")
@@ -32,13 +32,18 @@ const submitReview = asyncHandler(async (req,res)=>{
             book: new mongoose.Types.ObjectId(bookId)
         }
     )
+    await Book.findByIdAndUpdate(bookId,{
+        $push:{
+            reviews: review._id
+        }
+    })
     if (!review) {
         throw new ApiError(500,"Something went wrong while creating a review document ")
     }
     return res
     .status(200)
     .json(
-        new ApiError(200,review,"review is successfully submited")
+        new ApiResponse(200,review,"review is successfully submited")
     )    
 })
 
@@ -107,9 +112,60 @@ const updateReviewById = asyncHandler(async(req,res)=>{
     )
 })
 
+// get all the reviews
+const getAllReviews = asyncHandler(async(req,res)=>{
+    const {bookId} = req.params;
+    if (!isValidObjectId(bookId)) {
+        throw new ApiError(400,"Invalid book Id")
+    }
+
+    const reviews = await Review.aggregate([
+        {
+            $match:{
+                book: new mongoose.Types.ObjectId(bookId)
+            }
+        },
+        {
+            $lookup:{
+                from:"users",
+                foreignField:"_id",
+                localField:"owner",
+                as:"owner",
+                pipeline:[
+                    {
+                        $project:{
+                            _id:1,
+                            userename:1,
+                            fullname:1,
+                            avatar:1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields:{
+                owner:{
+                    $first:"$owner"
+                }
+            }
+        }
+    ])
+
+    if (!reviews.length) {
+        throw new ApiError(500,"Something went Wrong while fetching the reviews")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(200,reviews,"reviews successfully fetched")
+    )
+})
+
 
 export {
     submitReview,
     deleteReviewById,
-    updateReviewById
+    updateReviewById,
+    getAllReviews
 }
